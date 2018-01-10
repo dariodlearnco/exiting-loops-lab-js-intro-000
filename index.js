3874cd3e67541b6f14bd6ab032f035f029b11207
Dario Dorella <dario.dorella@gmail.com>
2018-01-10 22:34:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function breakOut(array, changeValue, stopValue) {
-    //  iterates through `array` and changes every element to `changeValue`
+    // Iterates through `array` and changes every element to `changeValue`
     // until the loop reaches `stopValue`. Then we `break` out of the loop and
     // return the array.
-    
+
     for (var i=0; i < array.length; i++) {
         if (array[i] === stopValue) {
             break
